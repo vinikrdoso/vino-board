a7730fdcde7c6d5005c62089d712be675e45985f
Vinicius Cardoso <vinicius.c.junqueira@aubay.pt>
2024-05-10 15:40:04 +0100

    feat: add room authentication

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,8 +2,9 @@ import { createClient } from '@liveblocks/client'
 import { createRoomContext, createLiveblocksContext } from '@liveblocks/react'
 
 const client = createClient({
-	publicApiKey:
-		'pk_dev_Ee8jLwwiCFl5R9Y_iMqiN7a0mBCZ-CSs-IRkBQ-ftiZIaA9hu74N86FYdsQ1ld-m',
+	authEndpoint: '/api/liveblocks-auth',
+	// publicApiKey:
+	// 	'pk_dev_Ee8jLwwiCFl5R9Y_iMqiN7a0mBCZ-CSs-IRkBQ-ftiZIaA9hu74N86FYdsQ1ld-m',
 	async resolveUsers({ userIds }) {
 		// Used only for Comments and Notifications. Return a list of user information
 		// retrieved from `userIds`. This info is used in comments, mentions etc.
@@ -67,8 +68,11 @@ type Storage = {
 // provided by your own custom auth back end (if used). Useful for data that
 // will not change during a session, like a user's name or avatar.
 type UserMeta = {
-	// id?: string,  // Accessible through `user.id`
-	// info?: Json,  // Accessible through `user.info`
+	id?: string
+	info?: {
+		name?: string
+		picture?: string
+	}
 }
 
 // Optionally, the type of custom events broadcast and listened to in this
