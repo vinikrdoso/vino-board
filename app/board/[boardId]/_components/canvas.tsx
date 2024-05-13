926049ebf4b94ce6c483c98a709a3d41a74c0643
Vinicius Cardoso <vinicius.c.junqueira@aubay.pt>
2024-05-13 11:51:25 +0100

    feat: add canvas state to toolbar options

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,20 +1,35 @@
 "use client"
 
+import { useState } from "react"
 import { Info } from "./info"
 import { Participants } from "./participants"
 import { Toolbar } from "./toolbar"
+import { CanvasMode, CanvasState } from "@/types/canvas"
+import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config"
 
 interface CanvasProps {
   boardId: string
-
 }
 
 export function Canvas({ boardId }: CanvasProps) {
+  const [canvasState, setCanvasState] = useState<CanvasState>({ mode: CanvasMode.None })
+
+  const { redo, undo } = useHistory()
+  const canRedo = useCanRedo()
+  const canUndo = useCanUndo()
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        redo={redo}
+        undo={undo}
+      />
 
     </main>
   )
